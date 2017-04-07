9d39386ea05067676f86289b2f456bc42578433d
Filip Vitas <felix557700@hotmail.com>
2017-04-07 23:32:57 +0200

    add better calendar and gulp tasks for building stylus

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp')
+var autoprefixer = require('gulp-autoprefixer')
+var concat = require('gulp-concat')
+var stylus = require('gulp-stylus')
+var sourcemaps = require('gulp-sourcemaps')
+var util = require('gulp-util')
+var cssnano = require('gulp-cssnano')
+var del = require('del')
+var runsequence = require('run-sequence')
+
+
+// for production: gulp build --env=production
+var env = util.env.env || 'development'
+
+gulp.task('clean', function () {
+    return del('./build')
+})
+
+gulp.task('css', function () {
+    return gulp.src(['./src/**/*.styl'])
+        .pipe(env === 'production' ? util.noop() : sourcemaps.init())
+        .pipe(stylus())
+        .pipe(env === 'production' ? util.noop() : sourcemaps.write())
+        .pipe(concat('bundle.css'))
+        .pipe(autoprefixer('last 3 versions'))
+
+        .pipe(env === 'production' ? cssnano() : util.noop())
+
+        .pipe(gulp.dest('build'))
+})
+
+gulp.task('watch', function () {
+    gulp.watch(['./src/**/*.styl'], ['css'])
+})
+
+gulp.task('default', function () {
+    runsequence('clean', 'css')
+})
--- main.js
+++ main.js
@@ -3,7 +3,15 @@ const electron = require('electron')
 const {app, BrowserWindow} = electron
 
 app.on('ready',  function () {
-    let mainWindow =  new BrowserWindow({ width: 800, height: 600})
+    let mainWindow =  new BrowserWindow({
+        useContentSize: true,
+        width: 400,
+        height: 300,
+        backgroundColor: 'white',
+        webPreferences: {
+            experimentalFeatures: true
+        }
+    })
     mainWindow.loadURL(`file://${__dirname}/index.html`)
 
     // mainWindow.webContents.openDevTools()
