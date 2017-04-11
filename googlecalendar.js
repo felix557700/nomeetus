4b167c625633e9b876232ef5bbedea2477e7b6a5
Filip Vitas <felix557700@hotmail.com>
2017-04-11 04:11:49 +0200

    add logic to authenticate user via google api and get data from calendar with just a token

--- googlecalendar.js
+++ googlecalendar.js
@@ -1,101 +1,116 @@
-var fs = require('fs');
-var readline = require('readline');
-var google = require('googleapis');
-var googleAuth = require('google-auth-library');
+const fs = require('fs');
+const Promise = require('bluebird')
+const readFile = Promise.promisify(require("fs").readFile);
+const readline = require('readline');
+const google = require('googleapis');
+const googleAuth = require('google-auth-library');
+const electron = require('electron')
+const {BrowserWindow} = electron
 
-var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 
 // at ~/.credentials/calendar-nodejs-quickstart.json
-var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/';
-var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
+const TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/';
+const TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
 
-// Load client secrets from a local file.
-fs.readFile('client_secret.json', function processClientSecrets(err, content) {
-    if (err) {
-        console.log('Error loading client secret file: ' + err);
-        return;
-    }
-    // Authorize a client with the loaded credentials, then call the Google Calendar API.
-    authorize(JSON.parse(content), listEvents);
-});
+function authorizeUser(credentials) {
+    var clientSecret = credentials.installed.client_secret
+    var clientId = credentials.installed.client_id
+    var redirectUrl = credentials.installed.redirect_uris[0]
+    var auth = new googleAuth()
+    var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl)
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-    var clientSecret = credentials.web.client_secret;
-    var clientId = credentials.web.client_id;
-    var redirectUrl = 'http://localhost:5000';
-    var auth = new googleAuth();
-    var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+    return new Promise(async function(resolve, reject) {
+        let token = await readTokenFromFile()
 
-    // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, function(err, token) {
-        if (err) {
-            getNewToken(oauth2Client, callback);
-        } else {
-            oauth2Client.credentials = JSON.parse(token);
-            callback(oauth2Client);
+        if (!token) {
+            // there is no token
+            let code = await getNewAuthCode(oauth2Client)
+
+            oauth2Client.getToken(code, function(error, token) {
+                if (error) {
+                    reject(error)
+                    return
+                }
+
+                storeToken(token)
+                oauth2Client.credentials = token
+                resolve(oauth2Client)
+            })
         }
+
+        oauth2Client.credentials = JSON.parse(token)
+        resolve(oauth2Client)
     });
 }
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
-function getNewToken(oauth2Client, callback) {
-    var authUrl = oauth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES
-    });
-    console.log('Authorize this app by visiting this url: ', authUrl);
-    var rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-    rl.question('Enter the code from that page here: ', function(code) {
-        rl.close();
-        oauth2Client.getToken(code, function(err, token) {
-            if (err) {
-                console.log('Error while trying to retrieve access token', err);
-                return;
-            }
-            oauth2Client.credentials = token;
-            storeToken(token);
-            callback(oauth2Client);
-        });
-    });
+function readTokenFromFile() {
+    return new Promise((resolve, reject) => {
+        readFile(TOKEN_PATH)
+            .then(token => resolve(token))
+            .catch(error => resolve())
+    })
 }
 
-/**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
+function getNewAuthCode(oauth2Client) {
+
+    return new Promise((resolve, reject) => {
+
+        const authUrl = oauth2Client.generateAuthUrl({
+            access_type: 'offline',
+            scope: SCOPES
+        })
+
+        let authWindow = new BrowserWindow({
+            width: 450,
+            height: 500,
+            useContentSize: true
+        })
+
+        authWindow.loadURL(authUrl)
+
+        authWindow.on('closed', function() {
+            authWindow = null
+            reject(new Error('User closed the window'))
+        })
+
+        authWindow.on('page-title-updated', function() {
+            setImmediate(() => {
+                const title = authWindow.getTitle()
+
+                if (title.startsWith('Denied')) {
+                    let code = title.split(/[ =]/)[2]
+                    reject(new Error(code))
+
+                    authWindow.removeAllListeners('closed')
+                    authWindow.close();
+                } else if (title.startsWith('Success')) {
+                    let code = title.split(/[ =]/)[2]
+                    resolve(code)
+
+                    authWindow.removeAllListeners('closed')
+                    authWindow.close()
+                }
+            })
+        })
+    })
+}
+
 function storeToken(token) {
     try {
-        fs.mkdirSync(TOKEN_DIR);
-    } catch (err) {
-        if (err.code != 'EEXIST') {
-            throw err;
+        fs.mkdirSync(TOKEN_DIR)
+    } catch (error) {
+        if (error.code !== 'EEXIST') {
+            throw error
         }
     }
-    fs.writeFileSync(TOKEN_PATH, JSON.stringify(token));
-    console.log('Token stored to ' + TOKEN_PATH);
+    fs.writeFileSync(TOKEN_PATH, JSON.stringify(token))
+    console.log('Token stored to ' + TOKEN_PATH)
 }
 
 function listEvents(auth) {
-    console.log('list events')
-    var calendar = google.calendar('v3');
+    var calendar = google.calendar('v3')
+
     calendar.events.list({
         auth: auth,
         calendarId: 'primary',
@@ -103,21 +118,23 @@ function listEvents(auth) {
         maxResults: 10, //TODO: list only 10 events
         singleEvents: true,
         orderBy: 'startTime'
-    }, function(err, response) {
-        if (err) {
-            console.log('The API returned an error: ' + err);
+    }, function(error, response) {
+        if (error) {
+            console.log('The API returned an error: ' + error)
             return;
         }
         var events = response.items;
         if (events.length === 0) {
-            console.log('No upcoming events found.');
+            console.log('No upcoming events found.')
         } else {
-            console.log('Upcoming 10 events:');
-            for (var i = 0; i < events.length; i++) {
-                var event = events[i];
-                var start = event.start.dateTime || event.start.date;
-                console.log('%s - %s', start, event.summary);
-            }
+            console.log('Upcoming 10 events:', events)
         }
-    });
+    })
+}
+
+module.exports.authorize = async function () {
+    readFile('client_secret.json')
+        .then(content => authorizeUser(JSON.parse(content)))
+        .then(authClient => listEvents(authClient))
+        .catch(error => console.log('Error loading client secret file: ' + error))
 }
